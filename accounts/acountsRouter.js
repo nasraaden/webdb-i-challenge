58099a79027bc6f75c73238465b5212fc7869573
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-01-21 10:44:25 -0800

    did queries and set up accounts router

---
+++ accounts/acountsRouter.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const db = require("../data/dbConfig.js");
+
+const router = express.Router();
+
+// get
+router.get("/", (req, res) => {
+
+})
+
+// get by id
+router.get("/:id", (req, res) => {
+
+})
+
+// post
+router.post("/", (req, res) => {
+
+})
+
+// put by id
+router.put("/:id", (req, res) => {
+
+})
+
+// delete by id
+router.delete("/:id", (req, res) => {
+
+})
+
+
+module.exports = router;
